d54c4080a86386e85de9b3c9d79aed410bc2ad8b
tl768 <58941783+tl768@users.noreply.github.com>
2020-05-10 23:48:09 +0800

    Update 2.再次编译.sh

--- 2.再次编译.sh
+++ 2.再次编译.sh
@@ -18,7 +18,8 @@ git pull
 ./scripts/feeds install -a
 
 #拷贝lean相关文件到r2s源内
-cp ~/friendlywrt-rk3328/lede/feeds.conf.default ~/friendlywrt-rk3328/friendlywrt/feeds.conf.default
+
+cp -rf ~/friendlywrt-rk3328/lede/feeds.conf.default ~/friendlywrt-rk3328/friendlywrt/feeds.conf.default
 
 cp -rf ~/friendlywrt-rk3328/lede/package/lean ~/friendlywrt-rk3328/friendlywrt/package/
 
@@ -37,9 +38,11 @@ cp -rf ~/friendlywrt-rk3328/lede/package/libs/librpc ~/friendlywrt-rk3328/friend
 cp -rf ~/friendlywrt-rk3328/lede/package/libs/libcxx ~/friendlywrt-rk3328/friendlywrt/package/libs/
 
 #修改lean源文件
+
 sed -i 's/#src-git helloworld/src-git helloworld/g' ~/friendlywrt-rk3328/friendlywrt/feeds.conf.default
 
 #更新融合后的源码
+
 cd ~/friendlywrt-rk3328/friendlywrt
 
 ./scripts/feeds update -a
@@ -47,27 +50,30 @@ cd ~/friendlywrt-rk3328/friendlywrt
 ./scripts/feeds install -a
 
 #导入配置文件(有配置文件时取消#)
+
 cat ~/friendlywrt-rk3328/configs/r2s.config > ~/friendlywrt-rk3328/friendlywrt/.config
 
 #编译固件
+
 cd ~/friendlywrt-rk3328/friendlywrt
 
 make -j1 download v=s
 
-cd ~/friendlywrt-rk3328
+cd ~/friendlywrt-rk3328/
 
 ./build.sh nanopi_r2s.mk
 
 cp ~/friendlywrt-rk3328/scripts/sd-fuse/out/FriendlyWrt_*_NanoPi-R2S_arm64_sd.img ~/
 
 #备份配置文件
+
 cp -rf ~/friendlywrt-rk3328/friendlywrt/.config ~/friendlywrt-rk3328/configs/r2s.config
 
 cp -rf ~/friendlywrt-rk3328/friendlywrt/.config ~/r2s.config
 
 #删除编译数据
 
-cd ~/friendlywrt-rk3328
+cd ~/friendlywrt-rk3328/
 
 ./build.sh cleanall
 
