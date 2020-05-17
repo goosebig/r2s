37f9d1daa3a59f0b31a2833bc4afacd7c3b1d72f
tl768 <58941783+tl768@users.noreply.github.com>
2020-05-18 02:19:43 +0800

    Update 1.首次编译.sh

--- 1.首次编译.sh
+++ 1.首次编译.sh
@@ -77,9 +77,6 @@ cd ~/friendlywrt-rk3328/friendlywrt/ && \
 #
 ./scripts/feeds install -a && \
 #
-#载入自定义固件配置文件
-cat ~/friendlywrt-rk3328/configs/r2s.config > ~/friendlywrt-rk3328/friendlywrt/.config;\
-#
 #编译固件
 cd ~/friendlywrt-rk3328/friendlywrt && \
 #
